12971be2b2c9f7d0831186654703ea4220becf2c
hazycora <hazycora@gmail.com>
2021-01-07 13:12:34 -0600

    changed "Join Zoom" to "Open Link"

--- open/code.js
+++ open/code.js
@@ -5,7 +5,7 @@ function addNew() {
   let itemTime = $('#fieldTime').val()
   let itemURL = $('#fieldURL').val()
 
-  let itemCode = '<div class="listItem" id="item'+itemNumb+'"><div id="itemDot" class="itemRow">◯</div><button type="button" onclick="removeItem(\'item'+itemNumb+'\')" class="alignEnd">Remove this item</button><br><br><p id="itemName" class="itemRow">'+itemName+'</p><p id="itemSep" class="itemRow"> - </p><p id="itemTime" class="itemRow">'+itemTime+'</p><a href="'+itemURL+'" id="itemURL" class="alignEnd"><p>Join Zoom</p></a></div>';
+  let itemCode = '<div class="listItem" id="item'+itemNumb+'"><div id="itemDot" class="itemRow">◯</div><button type="button" onclick="removeItem(\'item'+itemNumb+'\')" class="alignEnd">Remove this item</button><br><br><p id="itemName" class="itemRow">'+itemName+'</p><p id="itemSep" class="itemRow"> - </p><p id="itemTime" class="itemRow">'+itemTime+'</p><a href="'+itemURL+'" id="itemURL" class="alignEnd"><p>Open Link</p></a></div>';
 
   $('#sortable').append(itemCode);
   itemNumb += 1;
